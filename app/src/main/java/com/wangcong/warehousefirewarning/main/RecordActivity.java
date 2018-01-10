0ccb1144fd5e83f615936b18ebc56b5a4ef9e2a2
Wang Cong <1330792337@qq.com>
2018-01-10 16:49:45 +0800

    增加下拉刷新功能

--- app/src/main/java/com/wangcong/warehousefirewarning/main/RecordActivity.java
+++ app/src/main/java/com/wangcong/warehousefirewarning/main/RecordActivity.java
@@ -1,10 +1,10 @@
 package com.wangcong.warehousefirewarning.main;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import com.wangcong.warehousefirewarning.R;
 import com.wangcong.warehousefirewarning.utils.MyDatabaseUtil;
@@ -16,19 +16,28 @@ public class RecordActivity extends AppCompatActivity {
 
     private List<DataBean> recordList = new ArrayList<>();
     private MyDatabaseUtil database;
+    private RecordAdapter adapter;
+    private SwipeRefreshLayout swipe_refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
         initRecords();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-//        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
-        //layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        RecordAdapter adapter = new RecordAdapter(recordList);
+        adapter = new RecordAdapter(recordList);
         recyclerView.setAdapter(adapter);
+
+        swipe_refresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipe_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshRecords();
+            }
+        });
     }
 
     private void initRecords() {
@@ -37,6 +46,16 @@ public class RecordActivity extends AppCompatActivity {
         for (DataBean item : list) {
             recordList.add(item);
         }
-        Log.i(Const.TAG, "data size:" + recordList.size());
+//        Log.i(Const.TAG, "data size:" + recordList.size());
+    }
+
+    private void refreshRecords() {
+        recordList.clear();
+        List<DataBean> list = database.queryRecord();
+        for (DataBean item : list) {
+            recordList.add(item);
+        }
+        adapter.notifyDataSetChanged();
+        swipe_refresh.setRefreshing(false);
     }
 }
